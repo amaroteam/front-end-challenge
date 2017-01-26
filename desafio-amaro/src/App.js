41b11eea51ee02958b7e468fa9782755c68f8bd2
Rogerio Moura <roger0.rm@gmail.com>
2017-01-26 20:53:48 -0200

    Modified index.js for Routes, and App.js for menu's layout

--- desafio-amaro/src/App.js
+++ desafio-amaro/src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link } from 'react-router';
+import './css/pure-min.css';
+import './css/side-menu.css';
+import './css/products.css';
 
-class App extends Component {
+export default class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="layout">
+          <a href="#menu" id="menuLink" className="menu-link">
+              <span></span>
+          </a>
+
+          <div id="menu">
+              <div className="pure-menu">
+                  <Link className="pure-menu-heading" to="#">AMARO <span className='pure-menu-heading-inverted'>Week</span></Link>
+
+                  <ul className="pure-menu-list">
+                      <li className="pure-menu-item"><Link to="/" className="pure-menu-link">Home</Link></li>
+                      <li className="pure-menu-item"><Link to="/products" className="pure-menu-link">Produtos</Link></li>
+                      <li className="pure-menu-item"><Link to="/cart" className="pure-menu-link">Carrinho</Link></li>
+                  </ul>
+              </div>
+          </div>
+
+          <div id="main">
+              {this.props.children}
+          </div>
       </div>
     );
   }
 }
-
-export default App;
--- desafio-amaro/src/index.js
+++ desafio-amaro/src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import Home from './Home/';
+import Products from './Products';
+import Cart from './Cart';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+
+  (<Router history={browserHistory}>
+    <Route path='/' component={App}>
+      <IndexRoute component={Home} />
+      <Route path='/products' component={Products} />
+      <Route path='/cart' component={Cart} />
+    </Route>
+  </Router>),
   document.getElementById('root')
 );
