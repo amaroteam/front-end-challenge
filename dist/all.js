831f884a7c8437d24030677d217df3e6a9c973f3
Andre Sobral <andreqsobral@gmail.com>
2017-01-30 23:54:21 -0200

    grid and product tablet form

--- dist/all.js
+++ dist/all.js
@@ -13,26 +13,34 @@ Amaro.Product = (function() {
     var element = document.createElement('div');
 
     element.setAttribute("class", "Product");
-
-    if(this.image) {
-      var image = '<img class="Product__image" src="' + this.image + '">';
-    } else {
-      var image = '<img class="Product__image" src="dist/images/image-placeholder.svg">';
-    }
-
     element.innerHTML =
-      image +
+      this.getImage() +
       '<div class="Product__content">' +
         '<h3 class="Product__name">' +
           this.name +
         '</h3>' +
         '<div class="Product__price">' +
           '<span class="Product__price__number">' + this.price + '</span>' +
-        '</div>'
+          this.getDiscount() +
+        '</div>' +
       '</div>'
     ;
 
     parent.appendChild(element);
+  };
+
+  Product.prototype.getImage = function() {
+    if(this.image) {
+      return '<img class="Product__image" src="' + this.image + '">';
+    }
+
+    return '<img class="Product__image" src="dist/images/image-placeholder.svg">';
+  };
+
+  Product.prototype.getDiscount = function() {
+    if(this.discountPercentage) {
+      
+    }
   };
 
   return Product;
