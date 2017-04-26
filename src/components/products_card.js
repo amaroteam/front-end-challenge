ee25dea6a78ef2f8f514b3899a6b44907bdc1c4d
Guilherme Santos <guilherme.goncalvessantos@gmail.com>
2017-04-26 17:28:22 -0300

    improved card styles

--- src/components/products_card.js
+++ src/components/products_card.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 const ProductsCard = ({product}) => {
+  let imageEl = <div className="no-image">No Image :(</div>;
+
+  if (product.image) {
+    imageEl = <img src={product.image} />;
+  }
+
   return (
     <div className="product-card">
-      {/*<img src={product.image} />*/}
-      <div>{product.name}</div>
-      <div>{product.regular_price}</div>
-      <div>{product.actual_price}</div>
+      {imageEl}
+      <div className="product-name">{product.name}</div>
+      <div className={`product-regular-price ${(product.on_sale) ? 'sale' : ''}`}>{product.regular_price}</div>
+      <div className="product-price">{product.actual_price}</div>
     </div>
   );
 }
