7458f1f23b1d5872ef398d72a5055ec438e3507f
Bruno <bruno@syligo.com>
2017-06-16 00:21:26 -0300

    redux setup

--- amaro-challenge/src/index.js
+++ amaro-challenge/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
 import { getRoutes } from './routes';
+import configureStore from './store/configureStore';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+const store = configureStore();
 
 ReactDOM.render(
-  <Router routes={getRoutes()} history={browserHistory}/>
+  <Provider store={store}>
+    <Router routes={getRoutes()} history={browserHistory}/>
+  </Provider>
   , document.getElementById('root'));
 registerServiceWorker();
