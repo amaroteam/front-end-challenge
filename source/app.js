9e2ce452abbaab4b008dd2d0bcf74d35eaa73ee9
Gustavo Ribeiro <gusribeiro@gmail.com>
2018-06-20 12:17:04 -0300

    ✨ create card layout and components

--- source/app.js
+++ source/app.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import data from '../products.json';
+import Card from './components/card';
 
 class App extends React.Component {
 	render () {
 		return (
 			<div className="container">
+				<Card products={data.products} />
 			</div>
 		)
 	}
