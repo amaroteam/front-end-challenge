b61116b3824a647ed4c052cba709302f92d6773b
Vinicius Ribeiro <vinicius.vrf@gmail.com>
2018-12-02 14:43:16 -0200

    Add: Webfont Loader

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
 import App from './containers/App';
+import './styles/main.scss';
 import * as serviceWorker from './serviceWorker';
+
+WebFont.load({
+  google: {
+    families: ['Noto Sans SC:400,600,700', 'sans-serif'],
+  },
+});
 
 ReactDOM.render(
   <App />,
