4b87e5c3aadc85deec9e3abea06f6cad6e610d67
Vinicius Ribeiro <vinicius.vrf@gmail.com>
2018-12-11 06:03:19 -0200

    Add: Clear product action creator and products title
    - clearProduct thunk to clean product state in catalog page

--- src/containers/Products/index.jsx
+++ src/containers/Products/index.jsx
@@ -2,13 +2,27 @@ import React from 'react';
 import uuidv1 from 'uuid/v1';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { clearProductThunk } from '../../store/ducks/thunks';
 
 import Product from '../../components/Product';
 import './Products.scss';
 
-const Products = ({ products, loading }) => (
+const Products = ({
+  products,
+  loading,
+  clearProductConnected,
+}) => (
   <section className="products">
-    <div className="app__container">
+    <div className="app__container" onLoad={clearProductConnected}>
+      { products.length > 0
+        && (
+          <div className="header__title">
+            {`${products.length} items`}
+          </div>
+        )
+      }
+
       <div className="products__grid">
         { !loading
           && products.map(product => (
@@ -27,16 +41,22 @@ const Products = ({ products, loading }) => (
 Products.propTypes = {
   products: PropTypes.arrayOf(PropTypes.shape()),
   loading: PropTypes.bool,
+  clearProductConnected: PropTypes.func,
 };
 
 Products.defaultProps = {
   products: [],
   loading: true,
+  clearProductConnected: () => {},
 };
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  clearProductConnected: clearProductThunk,
+}, dispatch);
 
 const mapStateToProps = store => ({
   products: store.catalog.products,
   loading: store.catalog.loading,
 });
 
-export default connect(mapStateToProps)(Products);
+export default connect(mapStateToProps, mapDispatchToProps)(Products);
