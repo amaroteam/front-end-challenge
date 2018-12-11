a176b8af395077c27cf40d1f26479bcf2ca060bd
Vinicius Ribeiro <vinicius.vrf@gmail.com>
2018-12-11 05:35:24 -0200

    Add: Control icons
    - Arrow left icon
    - Plus icon (Quantity Control)
    - Minus icon (Quantity Control)

--- src/components/Button/IconButtons.jsx
+++ src/components/Button/IconButtons.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {
   FiSearch,
   FiShoppingBag,
+  FiArrowLeft,
+  FiPlus,
+  FiMinus,
 } from 'react-icons/fi';
 import PropTypes from 'prop-types';
 import Button from './index';
@@ -36,6 +39,57 @@ CartButton.propTypes = {
 };
 
 CartButton.defaultProps = {
+  className: '',
+  children: '',
+};
+
+export const BackButton = ({ children, ...props }) => (
+  <Button {...props}>
+    <FiArrowLeft />
+    {children}
+  </Button>
+);
+
+BackButton.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
+
+BackButton.defaultProps = {
+  className: '',
+  children: '',
+};
+
+export const PlusButton = ({ children, ...props }) => (
+  <Button {...props}>
+    <FiPlus />
+    {children}
+  </Button>
+);
+
+PlusButton.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
+
+PlusButton.defaultProps = {
+  className: '',
+  children: '',
+};
+
+export const MinusButton = ({ children, ...props }) => (
+  <Button {...props}>
+    <FiMinus />
+    {children}
+  </Button>
+);
+
+MinusButton.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
+
+MinusButton.defaultProps = {
   className: '',
   children: '',
 };
