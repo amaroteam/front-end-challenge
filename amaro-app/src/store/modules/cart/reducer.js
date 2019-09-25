9d1254a41bc6254750c917cf5c5d9d77382cee00
selique <judeudemolidor@hotmail.com>
2019-09-25 06:09:36 -0300

    Add listeners to reducer sagas

--- amaro-app/src/store/modules/cart/reducer.js
+++ amaro-app/src/store/modules/cart/reducer.js
@@ -4,15 +4,8 @@ export default function cart(state = [], action) {
   switch (action.type) {
     case '@cart/ADD_SUCCESS':
       return produce(state, draft => {
-        const productIndex = draft.findIndex(p => p.id === action.product.id);
-        if (productIndex >= 0) {
-          draft[productIndex].amount += 1;
-        } else {
-          draft.push({
-            ...action.product,
-            amount: 1,
-          });
-        }
+        const { product } = action;
+        draft.push(product);
       });
     case '@cart/REMOVE':
       return produce(state, draft => {
--- amaro-app/src/store/modules/cart/sagas.js
+++ amaro-app/src/store/modules/cart/sagas.js
@@ -1,13 +1,30 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 
 import api from '../../../services/api';
+import { formatPrice } from '../../../utils/format';
 
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmount } from './actions';
 
 function* addToCart({ id }) {
-  const response = yield call(api.get, `/products/${id}`);
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
 
-  yield put(addToCartSuccess(response.dataa));
+  if (productExists) {
+    const amount = productExists.amount + 1;
+
+    yield put(updateAmount(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.regular_price),
+    };
+
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
