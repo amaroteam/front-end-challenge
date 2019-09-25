887fc5a05e8aab34de679318c096250273a93219
selique <judeudemolidor@hotmail.com>
2019-09-25 05:53:46 -0300

    Configure and Add Reactotron-redux-saga

--- amaro-app/src/store/index.js
+++ amaro-app/src/store/index.js
@@ -4,7 +4,13 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMonitor =
+  process.env.NODE_ENV === 'development'
+    ? console.tron.createSagaMonitor()
+    : null;
+const sagaMiddleware = createSagaMiddleware({
+  sagaMonitor,
+});
 
 const enhancer =
   process.env.NODE_ENV === 'development'
