36b98e827f9a3e14fd9e47032500592ec21bcdfe
selique <judeudemolidor@hotmail.com>
2019-09-25 05:19:13 -0300

    Added basket status from items on button "add to cart"

--- amaro-app/src/pages/Home/index.js
+++ amaro-app/src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../utils/format';
 import api from '../../services/api';
@@ -9,14 +9,16 @@ import * as CartActions from '../../store/modules/cart/actions';
 import { ProductList } from './styles';
 
 export default function Home() {
-  const [productsData, setProductsData] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const response = await api.get('products');
+    setProducts(response.data);
+  };
+
   useEffect(() => {
-    async function fetchData() {
-      const response = await api.get('products');
-      setProductsData(response.data);
-    }
-    fetchData();
-  }, []);
+    fetchProducts(products);
+  }, [products]);
 
   const dispatch = useDispatch();
 
@@ -24,9 +26,16 @@ export default function Home() {
     dispatch(CartActions.addToCart(product));
   }
 
+  const amount = useSelector(state =>
+    state.cart.reduce((amount, product) => {
+      amount[product.id] = product.amount;
+      return amount;
+    }, {})
+  );
+
   return (
     <ProductList>
-      {productsData.map(product => (
+      {products.map(product => (
         <li key={product.id}>
           <img
             alt={product.name + ' - ' + product.color + ' - ' + product.slug}
@@ -37,6 +46,7 @@ export default function Home() {
           <button type="button" onClick={() => handleAddProduct(product)}>
             <div>
               <MdAddShoppingCart size={16} color="#000" />
+              {amount[product.id] || 0}
             </div>
             <span>Adicionar ao carrinho</span>
           </button>
