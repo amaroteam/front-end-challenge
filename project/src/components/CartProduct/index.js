f0d2e3d7ac67205fd1cf8983984eaec1acb15eeb
Sergio Kopplin <sergiokopplin@gmail.com>
2019-10-15 22:54:48 -0300

    removes warning messages

--- project/src/components/CartProduct/index.js
+++ project/src/components/CartProduct/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { string, bool, shape, number, func } from 'prop-types';
 
 import Icon from '../Icon';
