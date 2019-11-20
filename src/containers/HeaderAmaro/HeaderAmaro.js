c5efc1e6d60822ad37c270a07dbf61f6dd10188a
Rodrigo Reis <roreis99@gmail.com>
2019-11-20 19:25:42 -0300

    :chore: add configs globals sass and create first containers

---
+++ src/containers/HeaderAmaro/HeaderAmaro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/containers/HeaderAmaro.scss';
+
+const HeaderAmaro = () => {
+  return (
+    <header className="header">
+      <nav className="header__nav">Eae</nav>
+    </header>
+  );
+};
+
+export default HeaderAmaro;
