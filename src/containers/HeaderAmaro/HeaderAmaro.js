a21f0ac851ae35c3e0a8b49cd021cd6b22ee17bf
Rodrigo Reis <roreis99@gmail.com>
2020-01-25 10:42:01 -0300

    feat(Main): create object main

--- src/containers/HeaderAmaro/HeaderAmaro.js
+++ src/containers/HeaderAmaro/HeaderAmaro.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import '../../styles/containers/HeaderAmaro.scss';
 import { Link } from 'react-router-dom';
 import { ReactSVG } from 'react-svg';
 
-import Logo from '../../assets/icons/logo.svg';
+import '../../styles/containers/HeaderAmaro.scss';
+
+import LogoIcon from '../../assets/icons/logo.svg';
+import CartIcon from '../../assets/icons/shopping-bag.svg';
 
 const HeaderAmaro = () => {
   return (
     <header className="header">
-      <Link to="/">
-        <ReactSVG src={Logo} />
-      </Link>
+      <div className="header__top-bar">
+        <strong className="header__top-bar-text">
+          Amaro Teste Front-End
+        </strong>
+      </div>
+      <div className="header__container">
+        <Link to="/">
+          <ReactSVG src={LogoIcon} />
+        </Link>
+        <button className="header__container-cart" type="button">
+          <span className="header__container-cart-qty">5</span>
+          <ReactSVG src={CartIcon} />
+        </button>
+      </div>
     </header>
   );
 };
