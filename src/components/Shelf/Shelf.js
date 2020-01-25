6960c8963f8689fade20281f1799e5796dca4dab
Rodrigo Reis <roreis99@gmail.com>
2020-01-25 13:37:20 -0300

    feat(Shelf): create shelf component and styled her

---
+++ src/components/Shelf/Shelf.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import '../../styles/components/Shelf.scss';
+
+const Shelf = ({
+  image,
+  name,
+  installments,
+  actualPrice,
+  regularPrice,
+  discount,
+}) => {
+  return (
+    <div className="am-shelf">
+      <figure className="am-shelf__image">
+        <img src={image} alt={name} />
+      </figure>
+      <h4 className="am-shelf__name">{name}</h4>
+      <div className="am-shelf__pricing">
+        {discount ? (
+          <>
+            <span className="am-shelf__pricing-price">
+              {actualPrice}
+            </span>
+            {installments && (
+              <span className="am-shelf__pricing-installments">
+                {installments}
+              </span>
+            )}
+            <del className="am-shelf__pricing-old-price">
+              {regularPrice}
+            </del>
+            <span className="am-shelf__pricing-discount">
+              {`(${discount} off) `}
+            </span>
+          </>
+        ) : (
+          <>
+            <span className="am-shelf__pricing-price">
+              {actualPrice}
+            </span>
+            {installments && (
+              <span className="am-shelf__pricing-installments">
+                {installments}
+              </span>
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Shelf;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,6 +1,27 @@
 import React from 'react';
+import Container from '../../layout/Container';
+import Shelf from '../../components/Shelf';
 
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <h1>Home</h1>
+      <Shelf
+        image="https://res.cloudinary.com/amarotech/image/fetch/c_limit,f_auto,dpr_1,w_440,q_auto:best/v11576780009/https://cdn.amaro.com/images/products/20013513_296_original_1.jpg"
+        name="VESTIDO TRANSPASSE BOW"
+        actualPrice="R$ 199,90"
+        regularPrice="R$ 199,90"
+        installments="3x R$ 66,63"
+      />
+      <Shelf
+        image="https://d3l7rqep7l31az.cloudfront.net/images/products/20002581_614_catalog_1.jpg?1459536611"
+        name="VESTIDO TRANSPASSE BOW"
+        actualPrice="R$ 199,90"
+        regularPrice="R$ 199,90"
+        installments="3x R$ 66,63"
+        discount="12%"
+      />
+    </Container>
+  );
 };
 export default HomePage;
